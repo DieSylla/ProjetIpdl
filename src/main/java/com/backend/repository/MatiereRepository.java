4baafc57732a2cfbe8e23777e68a1685609c7fd9
DieSylla <diesylla202@gmail.com>
2025-06-17 11:13:22 +0000

    supression

--- src/main/java/com/backend/repository/MatiereRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.repository;
-
-import com.backend.model.Matiere;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatiereRepository extends JpaRepository<Matiere, Integer> {
-}
